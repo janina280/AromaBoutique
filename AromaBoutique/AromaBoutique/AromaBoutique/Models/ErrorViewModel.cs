e5070530e9d4e31174221681a09fdbdea7306035
Janina <coceijanina@yahoo.com>
2024-03-07 22:24:26 +0200

    Create Project Perfume

--- AromaBoutique/AromaBoutique/AromaBoutique/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AromaBoutique.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Perfume/Perfume/Controllers/BrandsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Perfume.Controllers
+{
+    public class BrandsController : Controller
+    {
+        public IActionResult Brand()
+        {
+            return View();
+        }
+    }
+}
