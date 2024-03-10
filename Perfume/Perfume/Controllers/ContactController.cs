c6464d6e57eef0314dbdf4d1a8b9f30f5350f95e
Janina <coceijanina@yahoo.com>
2024-03-10 13:58:22 +0200

    Contact

---
+++ Perfume/Perfume/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Perfume.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
