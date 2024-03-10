485528c0bfdd23d110c35592f9c2f051b7ff98d4
Janina <coceijanina@yahoo.com>
2024-03-10 20:28:56 +0200

    cart

---
+++ Perfume/Perfume/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Perfume.Models;
+
+namespace Perfume.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Cart()
+        {
+            var mockCart = new List<CartModel>()
+            {
+                new CartModel()
+                {
+                    BrandTitle = "Versace",
+                    Category = "Barbati",
+                    Currency = "RON",
+                    ImageSource = "~/images/onsale/D10-1.png",
+                    PerfumeTitle = "Test",
+                    Price = 470
+                }
+            };
+            return View(mockCart);
+        }
+    }
+}
---
+++ Perfume/Perfume/Models/CartModel.cs
@@ -0,0 +1,17 @@
+namespace Perfume.Models;
+
+public class CartModel
+{
+    public string ImageSource { get; set; }
+
+    public string BrandTitle { get; set; }
+
+    public string PerfumeTitle { get; set; }
+
+    public int Price { get; set; }
+    public string Category { get; set; }
+
+    public string Currency { get; set; }
+
+    
+}
