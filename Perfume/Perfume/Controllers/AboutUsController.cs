ee9952690ec827483d75d8d3ac600ddfbb0c6572
Janina <coceijanina@yahoo.com>
2024-03-10 04:19:32 +0200

    Review And Page Brand

---
+++ Perfume/Perfume/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Perfume.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
