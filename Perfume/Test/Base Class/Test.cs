95f90ce4165b11755172b46f3993b7e30a4eeace
janina280 <coceijanina@yahoo.com>
2024-06-12 11:10:37 +0300

    Create Automation Tests

---
+++ Perfume/Test/Base Class/Test.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+
+namespace Test.Base_Class
+{
+    public class Test
+    {
+        protected IWebDriver Driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            var chromeOptions = new ChromeOptions
+            {
+                AcceptInsecureCertificates = true
+            };
+
+            chromeOptions.AddArguments("--incognito");
+
+
+            chromeOptions.AddArgument("--start-maximized");
+
+
+            chromeOptions.AddUserProfilePreference("download.default_directory", path);
+
+            chromeOptions.AddArguments("--no-sandbox");
+            Driver = new ChromeDriver(path + @"\drivers\", chromeOptions);
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://localhost:7036/");
+
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Driver.Close();
+            Driver.Quit();
+        }
+    }
+}
---
+++ Perfume/Test/LoginTest.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class LoginTest:Base_Class.Test
+    {
+
+        [Test]
+        public async Task LoginSuccessTest()
+        {
+            Driver.Navigate().GoToUrl("https://localhost:7036/Account/Login");
+            await Task.Delay(7000);
+            var username = Driver.FindElement(By.Id("login_username"));
+            username.Clear();
+            username.SendKeys("admin@admin.ro");
+            var password = Driver.FindElement(By.Id("password_login"));
+            password.Clear();
+            password.SendKeys("Admin1234!");
+            var loginButton = Driver.FindElement(By.Id("login_button"));
+            loginButton.Click();
+            await Task.Delay(5000);
+            Assert.AreNotEqual("https://localhost:7036/Account/Login", Driver.Url);
+        }
+
+        [Test]
+        public async Task LoginWrongPasswordTest()
+        {
+
+            Driver.Navigate().GoToUrl("https://localhost:7036/Account/Login");
+            await Task.Delay(2000);
+            var username = Driver.FindElement(By.Id("login_username"));
+            username.Clear();
+            username.SendKeys("admin@admin.ro");
+            var password = Driver.FindElement(By.Id("password_login"));
+            password.Clear();
+            password.SendKeys("Admin123!");
+            var loginButton = Driver.FindElement(By.Id("login_button"));
+            loginButton.Click();
+            await Task.Delay(5000);
+            Assert.AreEqual("https://localhost:7036/Account/Login", Driver.Url);
+        }
+
+        [Test]
+        public async Task LoginWrongUsernameTest()
+        {
+
+            Driver.Navigate().GoToUrl("https://localhost:7036/Account/Login");
+            await Task.Delay(2000);
+            var username = Driver.FindElement(By.Id("login_username"));
+            username.Clear();
+            username.SendKeys("Ana");
+            var password = Driver.FindElement(By.Id("password_login"));
+            password.Clear();
+            password.SendKeys("Admin1234!");
+            var loginButton = Driver.FindElement(By.Id("login_button"));
+            loginButton.Click();
+            await Task.Delay(5000);
+            Assert.AreEqual("https://localhost:7036/Account/Login", Driver.Url);
+        }
+
+    }
+}
